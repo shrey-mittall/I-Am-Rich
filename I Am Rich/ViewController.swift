d8d9043c6b4e8ee35bf8ecb61065f51afabc7146
Shrey Mittal <shreymittal@gmail.com>
2020-01-09 23:10:56 -0800

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Shrey Mittal on 1/9/20.
+//  Copyright © 2020 Shrey Mittal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
